bb3e070e489454dac61e261ec859891e74281a8c
Keegan <kafalconar@gmail.com>
2023-11-10 09:01:25 +1000

    updated

--- main.py
+++ main.py
@@ -1,56 +1,50 @@
 from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup
-from newspaper import Article
 from textblob import TextBlob
-import time
 import json
 
 app = FastAPI()
 
 @app.get("/most_interesting_article")
 async def most_interesting_article():
-    url = "https://techcrunch.com/category/artificial-intelligence/"
+    category_url = 'https://techcrunch.com/category/artificial-intelligence/'
+    articles = get_article_links(category_url)
+    sentiments = {}
 
-    # Sending HTTP request
-    reqs = requests.get(url)
+    for article_url in articles:
+        title, content = get_article_details(article_url)
+        sentiment = analyze_sentiment(content)
+        sentiments[article_url] = (sentiment, title, content)
 
-    # Parsing the HTML
-    soup = BeautifulSoup(reqs.text, 'html.parser')
+    # Find the article with the best sentiment
+    best_article_url = max(sentiments, key=lambda x: sentiments[x][0])
+    best_article_sentiment, best_article_title, best_article_content = sentiments[best_article_url]
 
-    # Finding the articles by their HTML tag and class name
-    articles = soup.find_all('a', class_='post-block__title__link', limit=2)
+    # Construct a JSON object to return
+    best_article_data = {
+        "title": best_article_title,
+        "content": best_article_content
+    }
 
-    # Dictionary to store article information and sentiment
-    article_info = {}
+    return best_article_data
 
-    for i, article in enumerate(articles, start=1):
-        # Extracting the URL of the article
-        article_url = article.get('href')
+def get_article_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # The selector may need to be updated based on the website's structure
+    links = [a['href'] for a in soup.select('a.post-block__title__link')[:2]]
+    return links
 
-        # Using newspaper3k to extract information
-        article = Article(article_url)
-        article.download()
-        article.parse()
+def get_article_details(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # The selector for title may need to be updated based on the website's structure
+    title = soup.select_one('h1.article__title').get_text(strip=True)
+    # The selector for content may need to be updated based on the website's structure
+    article_text = ' '.join(p.get_text() for p in soup.select('div.article-content p'))
+    return title, article_text
 
-        # Delay for a moment to avoid sending too many requests in a short time
-        time.sleep(2)
-
-        # Analyzing sentiment
-        analysis = TextBlob(article.text)
-        sentiment_polarity = analysis.sentiment.polarity
-
-        # Storing information
-        article_info[i] = {
-            "title": article.title,
-            "url": article_url,
-            "content": article.text,
-            "sentiment": sentiment_polarity
-        }
-
-    # Finding the article with the highest sentiment polarity
-    most_interesting_article = max(article_info.items(), key=lambda x: x[1]['sentiment'])
-
-    # Returning the information as JSON
-    return most_interesting_article[1]
-
+def analyze_sentiment(text):
+    blob = TextBlob(text)
+    return blob.sentiment.polarity
